ae443d3f78c30706da9069a6da2918ce8e0785ac
sasha0552 <admin@sasha0552.org>
2023-03-29 23:08:52 +0600

    [FEAT] Add worker downloading, remove archive iteration & regex finder

--- src/index.js
+++ src/index.js
@@ -5,22 +5,23 @@ import { ReferenceWalker } from "./reference/reference-walker.js";
 import { CssReferenceFinder } from "./reference/impl/css-reference-finder.js";
 import { HtmlReferenceFinder } from "./reference/impl/html-reference-finder.js";
 import { JsReferenceFinder } from "./reference/impl/js-reference-finder.js";
-import { RegexReferenceFinder } from "./reference/impl/regex-reference-finder.js";
+
+/////
 
 import webpackCssChunksDetector from "./reference/impl/ast/detector/webpack-css-chunks-detector.js";
 import webpackJsChunksDetector from "./reference/impl/ast/detector/webpack-js-chunks-detector.js";
 import webpackResourceReferenceDetector from "./reference/impl/ast/detector/webpack-resource-reference-detector.js";
+import webpackWorkerReferenceDetector from "./reference/impl/ast/detector/webpack-worker-reference-detector.js";
 
 import webpackCssChunks from "./reference/impl/ast/compare/webpack-css-chunks.json" assert { type: "json"};
 import webpackJsChunks from "./reference/impl/ast/compare/webpack-js-chunks.json" assert { type: "json"};
 import webpackResourceReference from "./reference/impl/ast/compare/webpack-resource-reference.json" assert { type: "json"};
+import webpackWorkerReference from "./reference/impl/ast/compare/webpack-worker-reference.json" assert { type: "json"};
 
 import webpackCssChunksProcessor from "./reference/impl/ast/processor/webpack-css-chunks-processor.js";
 import webpackJsChunksProcessor from "./reference/impl/ast/processor/webpack-js-chunks-processor.js";
 import webpackResourceReferenceProcessor from "./reference/impl/ast/processor/webpack-resource-reference-processor.js";
-
-
-import { fetchIdList } from "./archive/archived-pages.js";
+import webpackWorkerReferenceProcessor from "./reference/impl/ast/processor/webpack-worker-reference-processor.js";
 
 /////
 
@@ -70,7 +71,8 @@ async function main() {
         new JsReferenceFinder(
             [
                 [ "webpackJsChunks", webpackJsChunksDetector, webpackJsChunks, webpackJsChunksProcessor ],
-                [ "webpackResourceReference", webpackResourceReferenceDetector,  webpackResourceReference,  webpackResourceReferenceProcessor ]
+                [ "webpackResourceReference", webpackResourceReferenceDetector,  webpackResourceReference,  webpackResourceReferenceProcessor ],
+                [ "webpackWorkerReference", webpackWorkerReferenceDetector,  webpackWorkerReference,  webpackWorkerReferenceProcessor ],
             ]
         )
     );
@@ -100,9 +102,8 @@ async function main() {
 
     /////
 
-    //for (const id of [ "latest" ].concat(await fetchIdList(DISCORDAPP_URL))) {
-    //for (const id of [ "20230101015410" ]) {
-    for (const id of [ "latest" ]) {
+    // https://web.archive.org/cdx/search/cdx?url=${url}
+    for (const id of [ "20230101015410", "20230201040033", "20230301005436" ]) {
         console.log("index.js: Start of fetching version with id %s", id);
         await walkAndSave(referenceWalker, id === "latest" ? DISCORDAPP_URL : `https://web.archive.org/web/${id}id_/${DISCORDAPP_URL}`, id);
         console.log("index.js: End of fetching version with id %s", id);
---
+++ src/reference/impl/ast/processor/webpack-worker-reference-processor.js
@@ -0,0 +1,22 @@
+import { findMarkersByName } from "../utils.js";
+
+/////
+
+export default function process(comparerResult) {
+    const markers = findMarkersByName(comparerResult, "workerReference");
+
+    /////
+
+    const references = [];
+
+    /////
+
+    for (const marker of markers) {
+        // TODO: fix hardcoded value
+        references.push(`/assets/${marker}`);
+    }
+
+    /////
+
+    return references;
+}
