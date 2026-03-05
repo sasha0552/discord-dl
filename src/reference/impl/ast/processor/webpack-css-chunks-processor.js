4f5c9e5653607216987192d57e77344ebc9885b4
sasha0552 <admin@sasha0552.org>
2026-03-05 05:19:56 +0000

    css fix

--- src/reference/impl/ast/processor/webpack-css-chunks-processor.js
+++ src/reference/impl/ast/processor/webpack-css-chunks-processor.js
@@ -13,6 +13,11 @@ export default function process(comparerResult) {
 
     for (const marker of markers) {
         for (const property of marker) {
+            // filter empty css chunks
+            if (property.value.value === "31d6cfe0d16ae931b73c") {
+                continue;
+            }
+
             // TODO: fix hardcoded value
             references.push(`/assets/${property.key.value}.${property.value.value}.css`);
         }
